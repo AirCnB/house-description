65c91e39644826c701abc5e891591e95c7121f78
Dussadee Wang <duss@Dussadees-MacBook-Pro.local>
2018-07-20 17:20:45 -0700

    Adds css style sheet

---
+++ client/component/highlights.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Highlights extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+
+
+
+
+
+
+
+
+
+
+
+  render() {
+  	return (
+  	  <div className='highlights'>
+        sup
+  	  </div>
+  	)
+  }
+}
+
+
+
+export default Highlights;
