2c043406f85fcb5f714a04d24e050d46e38122f1
Dussadee Wang <dw.gl80@gmail.com>
2018-07-24 15:40:41 -0700

    adds modal component and modal css file

---
+++ client/component/popup.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './popup.css';
+
+class Popup extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+
+
+
+
+
+
+
+
+  render() {
+  	return (
+  		<div>What's up</div>
+  	)
+  }
+}
