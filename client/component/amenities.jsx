2b6d3f1ab37e252ad721bb40956b6020b9788c71
Dussadee Wang <dw.gl80@gmail.com>
2018-07-24 22:16:17 -0700

    refactored css modules for amenities and popup

--- client/component/amenities.jsx
+++ client/component/amenities.jsx
@@ -4,12 +4,12 @@ import Popup from './popup.jsx';
 import styles from './amenities.css';
 
 const Amenities = (props) => (
-  <div>
+  <div className={styles.main}>
   	<div className={styles.title}>Amenities</div>
   	<div className={styles.wrapper}>
   	{props.info.amenities !== undefined ? props.info.amenities.map( item => 
       <div className={styles.icon}>
-  		  <img src={item.url} />
+  		  <img className={styles.image} src={item.url} />
   		  <span> {item.A}</span>
   		</div>) : null}
   	</div>
