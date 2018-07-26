caed194cb81591486d58338d8eaf7edf64bc305e
Dussadee Wang <dw.gl80@gmail.com>
2018-07-25 18:28:57 -0700

    cancellations component renders pers properly

--- client/component/cancellations.jsx
+++ client/component/cancellations.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import styles from './cancellations.css';
 
 const Cancellations = (props) => (
+  <div>
+    <div>Cancellations</div>
+    <div>Strict policy – Free cancellation within 48 hours</div>
+    <div>Cancel within 48 hours of booking to get a full refund.</div>
+    <div>Read more about the policy</div>
+
+  </div>
 );
 
 export default Cancellations;
