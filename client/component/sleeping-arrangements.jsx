a617b87e22d628250060b3bcad469b51d8599cbe
Dussadee Wang <dw.gl80@gmail.com>
2018-07-26 12:01:07 -0700

    changed ternary to inline && operator

--- client/component/sleeping-arrangements.jsx
+++ client/component/sleeping-arrangements.jsx
@@ -5,15 +5,13 @@ const SleepingArrangements = (props) => (
   <div className={styles.main}>
     <div className={styles.title}>Sleeping arrangements</div>
     <div className={styles.container}>
-    {props.info.sleep_arrangement ? props.info.sleep_arrangement.map( item => 
+    {props.info.sleep_arrangement && props.info.sleep_arrangement.map( item => 
       <div className={styles.box}>
       <img src='https://s3-us-west-1.amazonaws.com/front-end-capstone/double-king-size-bed.png' className={styles.icon} />
       <div className={styles.type}>{item === 'sofa bed' ? 'Common spaces' : `Bedroom ${props.info.bedroom_num}`}</div>
       <div className={styles.bed}>{item}</div>
       </div>
-    	) : null}
-      
-
+    	)}
     </div>
   </div>
 );
