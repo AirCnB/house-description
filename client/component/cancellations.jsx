967f9679f1497f573cfe2b210b514d77ca2097ea
Dussadee Wang <dw.gl80@gmail.com>
2018-07-25 18:59:43 -0700

    added css to space list properly

--- client/component/cancellations.jsx
+++ client/component/cancellations.jsx
@@ -7,12 +7,12 @@ const Cancellations = (props) => (
     <div className={styles.policy}>Strict policy – Free cancellation within 48 hours</div>
     <div className={styles.refund}>Cancel within 48 hours of booking to get a full refund.</div>
     <div className={props.showCancellation ? styles.visible : styles.hidden}>
-      <ul className={styles.list}>
-        <li>Cancel up to 24 hours before check-in to get a refund, minus Airbnb service fees.</li>
-        <li>Cancel within 24 hours of check-in and the first night is non-refundable.</li>
-        <li>Airbnb service fees are refunded only if the reservation is cancelled within 48 hours of booking and the guest has not checked-in yet.</li>
+      <ul>
+        <li className={styles.list}>Cancel up to 24 hours before check-in to get a refund, minus Airbnb service fees.</li>
+        <li className={styles.list}>Cancel within 24 hours of check-in and the first night is non-refundable.</li>
+        <li className={styles.list}>Airbnb service fees are refunded only if the reservation is cancelled within 48 hours of booking and the guest has not checked-in yet.</li>
       </ul>
-      <div className={styles.readMore}>Get details</div>
+      <div className={styles.getDetails}>Get details</div>
     </div>
     <div className={styles.readMore} onClick={props.toggleCancellation}>Read more about the policy</div>
   </div>
