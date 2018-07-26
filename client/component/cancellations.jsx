42c9c37ebb5df42f9e4031a60c20f26b6704ccb5
Dussadee Wang <dw.gl80@gmail.com>
2018-07-25 18:43:18 -0700

    added show cancellation policy toggle

--- client/component/cancellations.jsx
+++ client/component/cancellations.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './cancellations.css';
 
 const Cancellations = (props) => (
-  <div>
-    <div>Cancellations</div>
-    <div>Strict policy – Free cancellation within 48 hours</div>
-    <div>Cancel within 48 hours of booking to get a full refund.</div>
-    <div>Read more about the policy</div>
+  <div className={styles.main}>
+    <div className={styles.title}>Cancellations</div>
+    <div className={styles.policy}>Strict policy – Free cancellation within 48 hours</div>
+    <div className={styles.refund}>Cancel within 48 hours of booking to get a full refund.</div>
+    <div className={styles.readMore}>Read more about the policy</div>
 
   </div>
 );
