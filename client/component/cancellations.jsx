c8be3544adafe184c0d2a4fd5891c8bd5295dfe2
Dussadee Wang <dw.gl80@gmail.com>
2018-07-28 13:47:43 -0700

    add tests for cancellation component

--- client/component/cancellations.jsx
+++ client/component/cancellations.jsx
@@ -6,7 +6,7 @@ const Cancellations = (props) => (
     <div className={styles.title}>Cancellations</div>
     <div className={styles.policy}>Strict policy – Free cancellation within 48 hours</div>
     <div className={styles.refund}>Cancel within 48 hours of booking to get a full refund.</div>
-    <div className={props.showCancellation ? styles.visible : styles.hidden}>
+    <div id='cancellation' className={props.showCancellation ? styles.visible : styles.hidden}>
       <ul>
         <li className={styles.list}>Cancel up to 24 hours before check-in to get a refund, minus Airbnb service fees.</li>
         <li className={styles.list}>Cancel within 24 hours of check-in and the first night is non-refundable.</li>
---
+++ tests/cancellations.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Cancellations from '../client/component/cancellations.jsx';
+
+describe('Cancellations', () => {
+  it('should render component properly', () => {
+    const wrapper = shallow(<Cancellations />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it('runs function on click', () => {
+  	const mockFn = jest.fn();
+    const wrapper = shallow(<Cancellations toggleCancellation={mockFn} />)
+    wrapper.find('.readMore').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+  it('should have class visible when props.showCancellation is true', () => {
+    const wrapper = shallow(<Cancellations showCancellation={true} />)
+    expect(wrapper.find('#cancellation').hasClass('visible')).toEqual(true);
+  });
+  it('should have class hidden when props.showCancellation is false', () => {
+    const wrapper = shallow(<Cancellations showCancellation={false} />)
+    expect(wrapper.find('#cancellation').hasClass('hidden')).toEqual(true);
+  });
+  it('should have proper text when props.showCancellation is true', () => {
+    const wrapper = shallow(<Cancellations showCancellation={true} />)
+    expect(wrapper.find('.readMore').text()).toEqual('Hide policies ⌄');
+  })
+  it('should have proper text when props.showCancellation is false', () => {
+    const wrapper = shallow(<Cancellations showCancellation={false} />)
+    expect(wrapper.find('.readMore').text()).toEqual('Read more about the policy ⌄');
+  })
+});
